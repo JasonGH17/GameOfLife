dd26dd5474b4b918e606d308636b5ebfaa675de5
JasonGH17 <jason.ghantous@gmail.com>
2022-04-02 20:25:20 +0300

    Fixed neighbour assignment system to assign neighbour count to dead cells

--- src/Block.js
+++ src/Block.js
@@ -1,10 +1,16 @@
 class Block {
     constructor(state = 0) {
         this.state = state;  // 0 = Empty    1 = Filled
+
+        this.neighbours = 0;
     }
 
     setState(state) {
         this.state = state;
+    }
+
+    setNeighbours(count) {
+        this.neighbours = count;
     }
 
     get getState() {
--- src/emulate.js
+++ src/emulate.js
@@ -1,46 +1,34 @@
 function emulate(grid) {
-    const blocks = [];
-    for (let h = 0; h < grid.blocks.length; h++) {
-        for (let w = 0; w < grid.blocks[h].length; w++) {
-            if (grid.blocks[h][w].getState === 1)
-                blocks.push({ x: w, y: h, neighbours: 0 });
-        }
-    }
+    do {
+        for (let y = 0; y < grid.blocks.length; y++) {
+            for (let x = 0; x < grid.blocks[y].length; x++) {
+                let neighbours = 0;
 
-    while (true) {
-        for (let i in blocks) {
-            const block = blocks[i];
-            const x = block.x;
-            const y = block.y;
-            let neighbours = 0;
-
-            console.log(grid.blocks[y][x]);
-
-            if (y > 0) {
-                if (x > 0) {
-                    neighbours += grid.blocks[y - 1][x - 1].getState;
-                    neighbours += grid.blocks[y][x - 1].getState;
+                if (y > 0) {
+                    if (x > 0) {
+                        neighbours += grid.blocks[y - 1][x - 1].getState;
+                        neighbours += grid.blocks[y][x - 1].getState;
+                    }
+                    if (x < grid.blocks[y].length - 1) {
+                        neighbours += grid.blocks[y - 1][x + 1].getState;
+                        neighbours += grid.blocks[y][x + 1].getState;
+                    }
+                    neighbours += grid.blocks[y - 1][x].getState;
                 }
-                if (x < grid.blocks[y].length - 1) {
-                    neighbours += grid.blocks[y - 1][x + 1].getState;
-                    neighbours += grid.blocks[y][x + 1].getState;
+                if (y < grid.blocks.length - 1) {
+                    if (x > 0)
+                        neighbours += grid.blocks[y + 1][x - 1].getState;
+                    if (x < grid.blocks[y].length - 1)
+                        neighbours += grid.blocks[y + 1][x + 1].getState;
+                    neighbours += grid.blocks[y + 1][x].getState;
                 }
-                neighbours += grid.blocks[y - 1][x].getState;
+                
+                grid.blocks[y][x].setNeighbours(neighbours);
             }
-            if (y < grid.blocks.length - 1) {
-                if (x > 0)
-                    neighbours += grid.blocks[y + 1][x - 1].getState;
-                if (x < grid.blocks[y].length - 1)
-                    neighbours += grid.blocks[y + 1][x + 1].getState;
-                neighbours += grid.blocks[y + 1][x].getState;
-            }
-            blocks[i].neighbours = neighbours;
         }
 
-
-
-        setTimeout(() => { }, 250);
-    }
+        setTimeout(() => {console.log(grid.blocks)}, 250);
+    } while(false)
 }
 
 export default emulate;
