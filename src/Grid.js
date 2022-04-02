3346a582150e631f02050a1932864136b8824ebd
JasonGH17 <jason.ghantous@gmail.com>
2022-04-02 18:44:56 +0300

    Added function to construct a Grid from file

--- src/Grid.js
+++ src/Grid.js
@@ -1,4 +1,9 @@
 import Block from "./Block.js";
+import { readFileSync } from "fs";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 class Grid {
     constructor(width, height, charset) {
@@ -14,9 +19,13 @@ class Grid {
         for (let w = 0; w < this.width; w++) {
             this.blocks.push([]);
             for (let h = 0; h < this.height; h++) {
-                this.blocks[w].push(new Block(0));
+                this.blocks[w].push(new Block());
             }
         }
+    }
+
+    setBlock(x, y, state) {
+        this.blocks[x][y - 1].setState(state);
     }
 
     get grid() {
@@ -24,11 +33,30 @@ class Grid {
         for (let w = 0; w < this.width; w++) {
             grid.push([]);
             for (let h = 0; h < this.height; h++) {
-                grid[w].push(this.blocks[w][h].getState === 0 ? this.charset.empty : this.charset.filled);
+                grid[w].push(this.blocks[w][h].getState == 0 ? this.charset.empty : this.charset.filled);
             }
         }
         return grid;
     }
 }
+
+function constructFromFile(file) {
+    const json = JSON.parse(readFileSync(__dirname + file).toString());
+    const xy = Object.keys(json).length;
+
+    const grid = new Grid(xy, xy, { empty: " ", filled: "#" });
+
+    for (let h = 1; h <= xy; h++) {
+        for (let w = 1; w <= xy; w++) {
+            if (json[h][`${w}`])
+                grid.setBlock(h, w, json[h][`${w}`]);
+        }
+    }
+
+    return grid;
+};
+
+
+console.log(constructFromFile("\\grid.json").grid);
 
 export default Grid;
